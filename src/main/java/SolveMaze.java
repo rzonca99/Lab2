088450e9c27e1aa35728f730bf544a65fd6e2470
rzonca99 <erzonca2@illinois.edu>
2018-09-12 16:38:38 -0500

    I successfully implemented my group's maze-solving algorithm.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,16 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                if (maze.canMove() == true) {
+                    maze.move();
+                    maze.turnLeft();
+                } else {
+                    maze.turnRight();
+                }
+            }
+            System.out.println(maze);
+
         }
 
         if (maze.isFinished()) {
